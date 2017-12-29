55b25c9041304645a9d98c736706fdc464448239
Petr Čermák <30902390+cermak-petr@users.noreply.github.com>
2017-12-29 16:54:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const XLSX = require('xlsx');
 const Apify = require('apify');
 
 function toXlsxBuffer(array){
+    //flattenRecords(array)
     const ws = XLSX.utils.json_to_sheet(array);
     const wb = {SheetNames:['results'], Sheets:{'results': ws}};
     const wopts = {bookType:'xlsx', bookSST:false, type:'binary'};
@@ -19,17 +20,45 @@ function toXlsxBuffer(array){
     return s2ab(wbout);
 }
 
+function flattenRecord(record){
+    if(record && typeof record === 'object'){
+        for(const key in record){
+            const value = record[key];
+            if(value && Array.isArray(value)){
+                if(value.length > 0){
+                    record[key] = undefined;
+                    //delete record[key];
+                    flattenRecords(value);
+                    for(let i = 0; i < value.length; i++){
+                        record[key + '/' + i] = value[i];
+                    }
+                }
+            }
+        }
+    }
+}
+
+function flattenRecords(data){
+    for(let i = 0; i < data.length; i++){
+        flattenRecord(data[i]);
+    }
+}
+
 Apify.main(async () => {
     const input = await Apify.getValue('INPUT');
     let buffer;
-    if (Array.isArray(input)) {
+    
+    if(Array.isArray(input)){
         buffer = toXlsxBuffer(input);
-    } else if (input.storeId && input.key) {
+    }
+    else if(input.storeId && input.key){
         const jsonFromStore = await Apify.client.keyValueStores.getRecord(input);
         buffer = toXlsxBuffer(jsonFromStore.body);
-    } else {
+    }
+    else{
         throw new Error('Invalid input');
     }
+    
     const storeId = (await Apify.getEnv()).defaultKeyValueStoreId;
     const url = "https://api.apifier.com/v2/key-value-stores/" + storeId + "/records/results.xlsx?rawBody=1&disableRedirect=1";
     const type = "application/octet-stream";
